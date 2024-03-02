3e3c7f3b00f64dd62c7182727ffc8f98c220929c
Shivam <shivamr.cs.23@nitj.ac.in>
2024-03-02 21:51:46 +0530

    Initial application

---
+++ nodeServer/index.js
@@ -0,0 +1,28 @@
+//Node server hai
+
+const io= require('socket.io')(8000, {
+    cors: {
+    origin: "http://127.0.0.1:5500", // URL of your client-side application
+    methods: ["GET", "POST"]
+  }
+});
+
+const users={};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined',name =>{
+        console.log("New user",name)
+        users[socket.id]=name;
+        socket.broadcast.emit('user-joined',name);
+    });
+
+    socket.on('send',message =>{
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+})
+
